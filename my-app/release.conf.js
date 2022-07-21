dbd9ada75792ac306e7c35d73e9f6f07ba0c15d1
Tom Renner <tom.renner@navvis.com>
2022-07-21 10:24:02 +0200

    Correctly quote strings

--- my-app/release.conf.js
+++ my-app/release.conf.js
@@ -2,8 +2,8 @@ module.exports = {
     branches: "master",
     repositoryUrl: "https://github.com/tr325/github-actions-test",
     plugins: [
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
-        '@semantic-release/github'
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/github"
     ]
 }
