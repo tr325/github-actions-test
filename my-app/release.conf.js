455ab54b4f4aefb682fe48028803cdbc4a61ffd3
Tom Renner <tom.renner@navvis.com>
2022-07-21 10:47:42 +0200

    fix: Correctly upload coverage and build reports

--- my-app/release.conf.js
+++ my-app/release.conf.js
@@ -8,8 +8,8 @@ module.exports = {
             "@semantic-release/github",
             {
                 assets: [
-                    {path: "build.zip", label: "Build"},
-                    {path: "coverage.zip", label: "Test coverage"},
+                    {path: "my-app/build.zip", label: "Build"},
+                    {path: "my-app/coverage.zip", label: "Test coverage"},
                 ],
             },
         ],
