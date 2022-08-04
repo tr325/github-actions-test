f51e7427ad34bbd0907c47458a21480c4b84d587
Tom Renner <tom.renner@navvis.com>
2022-08-04 16:16:35 +0200

    feat: explore more oof the github core package features

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -926,14 +926,26 @@ try {
 	// throw(new Error("some error message"));
 
 
+	core.debug("Debug message");		// Only logs if ACTION_STEP_DEBUG = true, set in GH secrets'
+	core.warning("Warning message");	
+	core.error("Warning message");		// Won't fail the job, but will log in RED
+
 	const name = core.getInput("who-to-greet");
+	core.setSecret(name); 		// won't be logged
 	console.log(`Hello ${name}`);
 
 	const time = new Date();
 	core.setOutput("time", time.toTimeString());
 
 
+	// Log large JSON object in an expandable group.
+	core.startGroup("Logging Github object");
 	console.log(JSON.stringify(github, null, 2));
+	core.endGroup();
+
+
+	core.exportVariable("HELLO", "hi");  	// Sets an env var on the runner VM
+
 } catch (e) {
 	// Need to explicitly set the action as failed in the core package, even if it throws
 	core.setFailed("We threw an error or something");
--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -3,18 +3,30 @@ const github = require("@actions/github");
 
 
 try {
-	// throw(new Error("some error message"));
+    // throw(new Error("some error message"));
 
 
-	const name = core.getInput("who-to-greet");
-	console.log(`Hello ${name}`);
+    core.debug("Debug message");        // Only logs if ACTION_STEP_DEBUG = true, set in GH secrets'
+    core.warning("Warning message");
+    core.error("Warning message");      // Won't fail the job, but will log in RED
 
-	const time = new Date();
-	core.setOutput("time", time.toTimeString());
+    const name = core.getInput("who-to-greet");
+    core.setSecret(name);       // won't be logged
+    console.log(`Hello ${name}`);
+
+    const time = new Date();
+    core.setOutput("time", time.toTimeString());
 
 
-	console.log(JSON.stringify(github, null, 2));
+    // Log large JSON object in an expandable group.
+    core.startGroup("Logging Github object");
+    console.log(JSON.stringify(github, null, 2));
+    core.endGroup();
+
+
+    core.exportVariable("HELLO", "hi");     // Sets an env var on the runner VM
+
 } catch (e) {
-	// Need to explicitly set the action as failed in the core package, even if it throws
-	core.setFailed("We threw an error or something");
+    // Need to explicitly set the action as failed in the core package, even if it throws
+    core.setFailed("We threw an error or something");
 }
